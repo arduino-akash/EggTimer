6dc9371a6f973b4d3a5fda0d28e1d8a051933ea5
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-10-17 16:01:16 +0530

    Commit - 2

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        print(result)
+        
+    }
 }
